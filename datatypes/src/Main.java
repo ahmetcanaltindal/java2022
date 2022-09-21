23dfaf8a5d4229d5a1cfe7c38d68ed5a68cd0879
ahmet <ahmet@DESKTOP-CVJPJHM.home>
2022-09-21 20:14:00 +0300

    c

---
+++ datatypes/src/Main.java
@@ -0,0 +1,20 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int sayi = 5;
+		double ondaliklisayi = 12.9;
+		char karakter = 'a';
+		boolean dogrumu=false;
+		
+		
+		System.out.println("sayi : " + sayi );
+		System.out.println("küsaratli : " + ondaliklisayi);
+		System.out.println("karakter :" + karakter);
+		System.out.println("dogrumu : " + dogrumu);
+
+	}
+
+}
