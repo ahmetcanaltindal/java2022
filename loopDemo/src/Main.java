05b27df529564793657ddef342b49bbfa54f420c
ahmet <ahmet@DESKTOP-CVJPJHM.home>
2022-09-21 20:13:07 +0300

    co

---
+++ loopDemo/src/Main.java
@@ -0,0 +1,35 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		//for
+		for(int i=0; i<10; i+=2) {
+			System.out.println(i);
+		}
+		System.out.println(" For dongusu bitti");
+		
+		
+		//while
+		int i =20;
+		while(i<10) {
+			System.out.println(i);
+			i++;
+		}
+		
+		System.out.println(" While dongusu bitti");
+		
+		//do While
+		int j=20;
+				
+		do {
+			System.out.println(j);
+			j+=2;
+			
+					
+		}while (j<10);
+		System.out.println("do While dongusu bitti");
+
+	}
+
+}
