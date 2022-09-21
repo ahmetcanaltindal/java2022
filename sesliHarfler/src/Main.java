a6328eb9534f0c5c4364365b4157668dc75a273a
ahmet <ahmet@DESKTOP-CVJPJHM.home>
2022-09-21 20:09:16 +0300

    commit

---
+++ sesliHarfler/src/Main.java
@@ -0,0 +1,24 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		char harf = 'A';
+		
+		switch(harf) {
+		case 'A':
+		case 'I':
+		case 'O':
+		case 'U':
+			System.out.println("Kalın sesli harf");
+			break;
+		default:
+			System.out.println("İnce sesli harf");
+		
+		
+		}
+
+	}
+
+}
